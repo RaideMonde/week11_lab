16702663e329b728cf7a5e66d31ead292944d65d
Raidemonde <i.am.rahul@live.com>
2021-03-26 20:19:07 -0700

    Made todoList and completedTodos and put some items in them

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,13 @@
-// your JavaScript goes here, you can delete this comment
+
+let todoList = [
+                { todo: "clone repo for starter code", status:"complete" },
+                { todo: "make a todo list", status: "complete"}
+            ];
+
+let completedTodos = [
+                    { todo: "Dance", status: "complete"},
+                    { todo: "Complete week 11 lab", status: "complete"}
+            ];
+
+            
+
